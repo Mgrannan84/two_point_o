82ea88b45af8b20ec3c21f055738b7ff30b4046a
Michael Grannan <michael.grannan@hotmail.com>
2018-05-15 21:02:06 +0000

    create action to save to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,22 @@
 class ContactsController < ApplicationController
-    def new
+  
+  def new
+      @contact = Contact.new
+  end
+  
+def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    ##flash[:success] = 'Message sent.'
+    redirect_to new_contact_path, notice: "Message Sent."
+  else
+    ##flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path, notice: "Error Occured."
+  end
+end
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
     end
+  
 end
