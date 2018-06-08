1a07abd9f43382be7751b9e8f021c0c130ef4f22
Michael Grannan <michael.grannan@hotmail.com>
2018-06-08 03:51:57 +0000

    added edit profile view and action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,13 @@ def create
   end
 end
 
+
+def edit
+  #for get request made to /user/:user_id/profile/edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+end
+
 private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
